2d3542cf6d0e384aa2105358016746f520eee103
Igor Souto <igor_souto@outlook.com>
2018-08-21 18:00:48 -0300

    166

--- challenge-16/challenge-16.js
+++ challenge-16/challenge-16.js
@@ -19,10 +19,11 @@ console.log( 'As letras do seu nome:' );
 
 var name = 'igor';
 
-console.log(name.charAt(0) + ' é a ' + (name.indexOf('i')+1) + 'º letra do meu nome.');
-console.log(name.charAt(1) + ' é a ' + (name.indexOf('g')+1) + 'º letra do meu nome.');
-console.log(name.charAt(2) + ' é a ' + (name.indexOf('o')+1) + 'º letra do meu nome.');
-console.log(name.charAt(3) + ' é a ' + (name.indexOf('r')+1) + 'º letra do meu nome.');
+for(var i = 0; i < name.length; i++){
+    console.log(name.charAt(i) + ' é a ' + (i + 1) + 'º letra do meu nome.');
+}
+
+
 
 /*
 - Declare uma variável chamada `fullName`, que receba seu nome completo,
@@ -40,25 +41,18 @@ console.log( '\nNome convertido à partir de um slug:' );
 
 var fullName = 'igor-souto-de-souza';
 
-function captalizeName(name){
+var newFullName = fullName.split('-');
 
-    name = name.split('-').join(' ');
-    console.log(name.charAt((name.indexOf(' ')+1)).toUpperCase() );
+newFullName = newFullName.map(function(item){
 
-   
+    return item.charAt(0).toUpperCase() + item.slice(1);
 
+    });
 
-
-
-
-    return name;
-
-}
-
-
-
-console.log(fullName);
-console.log(captalizeName(fullName));
+newFullName = newFullName.join(' ');
+  
+    console.log(newFullName);
+    
 /*
 - Crie um array com 5 nomes. Reduza esses nomes a uma única string, separando
 cada nome por vírgula. Entre o penúltimo e o último nome, o separador deve
@@ -69,21 +63,37 @@ O resultado final deve ficar mais ou menos assim:
 5 nomes foi somente uma sugestão ;)
 */
 console.log( '\nMeus amigos:' );
-// ?
+
+var arrNames = ['Igor', 'Felipe', 'Giovanna', 'Luigi', 'Alef'];
+
+
+var newArrNames = arrNames.reduce(function(acumulado,atual,index){
+
+    var separator = arrNames.length - 1 === index ? ' e ' : ', ';
+
+    return acumulado + separator + atual;    
+
+});
+  
+
+
+console.log(newArrNames);
 
 /*
 Usando o replace(), faça a string "Roberto" virar "Roberta".
 Mostre o resultado no console.
 */
 console.log( '\nEra "Roberto", agora é:' );
-// ?
+
+console.log('Roberto'.replace('to','ta'));
 
 /*
 Mostre no console a parte "nando" da string "Fernando". Use o método que
 faz a busca do final para o início da string.
 */
 console.log( '\nParte de uma string:' );
-// ?
+
+console.log('Fernando'.substring(8,3 ));
 
 /*
 Declare uma variável chamada `myName`, que receba o seu primeiro nome,
